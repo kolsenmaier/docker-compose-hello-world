aab7c53a36c5f4de0cf9e71627b032a1cca99a26
Olsen-Maier <kolsenmai@cavc-ofcmc0141.local>
2018-10-20 20:41:10 -0700

    Removed redis to resolve heroku issues

--- app.py
+++ app.py
@@ -1,30 +1,11 @@
-import time
-
-import redis
+import os
 from flask import Flask
-
-
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
 
-
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
-
-
-@app.route('/')
+@app.route("/")
 def hello():
-    count = get_hit_count()
-    return 'Hello World! This message has been seen {} times.\n'.format(count)
+    return "Hello from Python!"
 
 if __name__ == "__main__":
-    from os import environ
-    app.run(host=environ.get("HTTP_HOST", '0.0.0.0'), port=environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
