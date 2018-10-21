8285b9d5f42b914c61982c73b74e1641de0d333c
Olsen-Maier <kolsenmai@cavc-ofcmc0141.local>
2018-10-20 21:09:16 -0700

    Restored redis code, updated docker-compose ports

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 import os
+import time
+import redis
 from flask import Flask
-app = Flask(__name__)
 
-@app.route("/")
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+
+@app.route('/')
 def hello():
-    return "Hello from Python!"
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=port)
