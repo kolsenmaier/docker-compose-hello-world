26b3dd1f8e016111bf547d57adb9bc32eaae2cfa
Olsen-Maier <kolsenmai@cavc-ofcmc0141.local>
2018-10-20 21:26:51 -0700

    Removed redis, for good

--- app.py
+++ app.py
@@ -1,28 +1,12 @@
 import os
-import time
-import redis
 from flask import Flask
 
 app = Flask(__name__)
-port = int(os.environ.get("PORT", 5000))
-cache = redis.Redis(host='redis', port=port)
-
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
-
 
 @app.route('/')
 def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return 'Hello World!\n'
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
